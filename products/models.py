b1260cf031bb7c8b879296303cd0a5ffbaa82cf6
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-07-31 08:16:46 -0300

    created base Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title       = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits=9)
+    featured    = models.BooleanField()
+    
