0767977f22a566ffc3faf99ca0eff58fe2d6904d
lobinjeje <86988207+lobinjeje@users.noreply.github.com>
2021-08-01 18:05:52 -0300

    updated navbar, burguer menu now running, added functions at script.js

---
+++ static/navbar/javascript/script.js
@@ -0,0 +1,14 @@
+// navbar toggle
+const toggleBtn = document.getElementById('navbar-toggler');
+const navDiv = document.querySelector('.navbar-collapse');
+
+toggleBtn.addEventListener('click', function(){
+    navDiv.classList.toggle('showNav');
+    if(toggleBtn.firstElementChild.className = "fas fa-bars fa-fw"){
+        toggleBtn.firstElementChild.className = "fas fa-times fa-fw";
+        document.body.style.overflow = "hidden";
+    } else{
+        toggleBtn.firstElementChild.className = "fas fa-bars fa-fw";
+        document.body.style.overflow = "visible";
+    }
+});
