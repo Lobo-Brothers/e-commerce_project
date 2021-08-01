ef4f4dd2cba29214d420d15a7242a3a3d260f5bb
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-01 07:32:08 -0300

    added superuser checker comment

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
+    
+
     return render(request, "home.html", {})
+
+'''if request.user.is_superuser == True:
+        return HttpResponse('You are super user.')
+    else:
+        return HttpResponse('Fuck you.')'''
