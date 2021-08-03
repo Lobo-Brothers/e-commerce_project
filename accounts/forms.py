1c817c9a09f64dbd395816e7bff841b68d7747f3
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-03 04:29:45 -0300

    created user_Registration form.

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,3 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import forms
 
+class RegisterUserForm(UserCreationForm):
+    class Meta:
+        model   = User
+        fields  = ['username', 'email', 'password1', 'password2']
