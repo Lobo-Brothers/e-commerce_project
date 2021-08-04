3f5e682e9f807f858544b5e13d06e257fc2de31d
lobinjeje <86988207+lobinjeje@users.noreply.github.com>
2021-08-03 22:09:59 -0300

    updated slider buttons
    now on click have a color

--- static/homepage/javascript/script-home.js
+++ static/homepage/javascript/script-home.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll('.slide');
 var buttons = document.querySelectorAll('.btn');
-let currentSlide = 1;
+const currentSlide = 1;
 
 var manualNav = function(manual){
     slides.forEach((slide) => {
@@ -20,3 +20,30 @@ buttons.forEach((btn, i) => {
         currentSlide = i;
     });
 });
+
+var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    var repeater = () => {
+        setTimeout(function(){
+            [...active].forEach((activeSlide) => {
+                activeSlide.classList.remove('active');
+            })
+
+            slides[i].classList.add('active');
+            buttons[i].classList.add('active');
+            i++;
+
+            if(slides.length == i){
+                i = 0;
+            }
+            if(i >= slides.lenght){
+                return;
+            }
+            repeater();
+        }, 10000);
+    }
+    repeater();
+}
+repeat();
