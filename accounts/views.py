fbebb48b32e262afa090965f5194ac1c986aee57
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-05 18:46:23 -0300

    added user registration

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from .forms import RegisterUserForm
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 # Create your views here.
 
 def user_registration_view(request):
@@ -11,6 +12,7 @@ def user_registration_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect(user_login_view)
     
     context = {'form': form}
     return render(request, 'accounts/user_registration.html', context)
