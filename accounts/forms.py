d2ac57329b4396b84bc94152a24c2f8c6c9c2a15
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-08 09:39:46 -0300

    updated accounts form

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,10 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import forms
+from django.forms.models import fields_for_model
 
 class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField()
     class Meta:
         model   = User
         fields  = ['username', 'email', 'password1', 'password2']
