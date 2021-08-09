c3ce79be562a36a803141d3b82c4b22f629b9144
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-09 08:47:01 -0300

    updated urls.py and accounts template dierctory

--- my_website/urls.py
+++ my_website/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
 
     # Accounts app
     path('register/', user_registration_view, name='registration_page'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
