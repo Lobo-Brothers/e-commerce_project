3aa27d9423c5c80b7947646038048cf925e89702
lobinjeje <86988207+lobinjeje@users.noreply.github.com>
2021-08-14 23:12:20 -0300

    RE-DO CSS ARCHIVE AND MORE UPDATES
    i made one unique css doc, where now i am loading all the contents. i added a list of pre-order products as cards. fixed media queries bugs, messy codes and i added new comments where i mention what i am editing

--- static/navbar/javascript/scripts.js
+++ static/navbar/javascript/scripts.js
@@ -5,10 +5,18 @@ const navDiv = document.querySelector('.navbar-collapse');
 toggleBtn.addEventListener('click', function(){
     navDiv.classList.toggle('showNav');
     if(toggleBtn.firstElementChild.className = "fas fa-bars fa-fw"){
-        toggleBtn.firstElementChild.className = "fas fa-times fa-fw";
         document.body.style.overflow = "hidden";
     } else{
         toggleBtn.firstElementChild.className = "fas fa-bars fa-fw";
         document.body.style.overflow = "visible";
     }
 });
+
+// search box
+
+const icon = document.getElementById('search-btn');
+const search = document.getElementById('search');
+
+icon.onclick = function(){
+    search.classList.toggle('active')
+}
