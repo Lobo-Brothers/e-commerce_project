d4f968603be3bd7446ee620ef4d959980f6eac88
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-16 20:07:08 -0300

    updated accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,22 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
     class Meta:
         model   = User
         fields  = ['username', 'email', 'password1', 'password2']
+    
+    username    = forms.CharField(required=True, max_length=32, widget=forms.TextInput(attrs={
+            'placeholder': 'Username',
+            'class': 'signup-labels'
+        }))
+    email       = forms.EmailField(required=True, widget=forms.TextInput(attrs={
+            'placeholder': 'your@email.com',
+            'class': 'signup-labels'
+        }))
+    password1   = forms.CharField(required=True, max_length=64, widget=forms.PasswordInput(attrs={
+            'class': 'signup-labels'
+        }))
+
+    password2   = forms.CharField(required=True, max_length=64, widget=forms.PasswordInput(attrs={
+            'class': 'signup-labels'
+        }))
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterUserForm
 
 from django.shortcuts import redirect
