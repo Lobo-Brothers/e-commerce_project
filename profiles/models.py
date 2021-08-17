c63531c5402546b0b61036e96f368a0b5ad87fc1
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-17 01:58:08 -0300

    updated profiles models.py

--- profiles/models.py
+++ profiles/models.py
@@ -8,9 +8,9 @@ from .choices import TOP_SIZES
 
 class Profile(models.Model):
     user                = models.OneToOneField(User, on_delete=models.CASCADE)
-    name_and_last_name  = models.CharField(max_length=48, null=True)
-    top_sizes           = models.CharField(null=True, max_length=3, choices=TOP_SIZES)
-    shoes_sizes         = models.PositiveIntegerField(null=True, validators=[MinValueValidator(17), MaxValueValidator(48)])
+    name_and_last_name  = models.CharField(max_length=48, blank=True)
+    top_sizes           = models.CharField(blank=True, max_length=3, choices=TOP_SIZES)
+    shoes_sizes         = models.PositiveIntegerField(blank=True, validators=[MinValueValidator(17), MaxValueValidator(48)])
     date_created        = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -18,10 +18,10 @@ class Profile(models.Model):
 
 class Billaddress(models.Model):
     user                = models.ForeignKey(User, on_delete=models.CASCADE)
-    postal_code         = models.CharField(max_length=16)
+    postal_code         = models.CharField(max_length=16, blank=True)
     country             = CountryField()
-    city                = models.CharField(max_length=64)
-    address             = models.CharField(max_length=64, null=True)
+    city                = models.CharField(max_length=64, blank=True)
+    address             = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return f'{self.address}, {self.city}, {self.postal_code}'
