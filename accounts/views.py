061801cf35db41790ee1d4ba240000261424e048
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-16 21:34:41 -0300

    changed login view :)

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 
-from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterUserForm
 
 from django.shortcuts import redirect
 
 from django.contrib import messages
+
+#Login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
@@ -24,3 +28,26 @@ def user_registration_view(request):
 
     context = {'form': form}
     return render(request, 'accounts/user_registration.html', context)
+
+def login_view(request):
+    my_form    = AuthenticationForm(request.POST)
+
+    if request.method == 'POST':
+        my_form    = AuthenticationForm(request, request.POST)
+
+        if my_form.is_valid():
+            username    = my_form.cleaned_data.get('username')
+            password    = my_form.cleaned_data.get('password')
+            user        = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Succesfully logged in')
+                return redirect('/')
+            else:
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'pelotudo de mierda pusiste algo mal')
+
+    context = {'form': my_form}
+    return render(request, 'accounts/login.html', context)
