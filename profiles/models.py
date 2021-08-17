a9e96689c2ab0d9f0f30896d0c0b1fd82084c10d
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-17 01:41:30 -0300

    updated database

--- profiles/models.py
+++ profiles/models.py
@@ -7,11 +7,11 @@ from django_countries.fields import CountryField
 from .choices import TOP_SIZES
 
 class Profile(models.Model):
-    user            = models.OneToOneField(User, on_delete=models.CASCADE)
-    name            = models.CharField(max_length=64, null=True, blank=True)
-    top_sizes       = models.CharField(null=True, blank=True, max_length=3, choices=TOP_SIZES)
-    shoes_sizes     = models.PositiveIntegerField(null=True, blank=True, validators=[MinValueValidator(17), MaxValueValidator(48)])
-    date_created    = models.DateField(auto_now_add=True)
+    user                = models.OneToOneField(User, on_delete=models.CASCADE)
+    name_and_last_name  = models.CharField(max_length=48, null=True)
+    top_sizes           = models.CharField(null=True, max_length=3, choices=TOP_SIZES)
+    shoes_sizes         = models.PositiveIntegerField(null=True, validators=[MinValueValidator(17), MaxValueValidator(48)])
+    date_created        = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user}, {self.name}'
@@ -21,7 +21,7 @@ class Billaddress(models.Model):
     postal_code         = models.CharField(max_length=16)
     country             = CountryField()
     city                = models.CharField(max_length=64)
-    address             = models.CharField(max_length=64)
+    address             = models.CharField(max_length=64, null=True)
 
     def __str__(self):
         return f'{self.address}, {self.city}, {self.postal_code}'
