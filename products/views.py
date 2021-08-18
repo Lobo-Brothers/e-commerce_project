758aef0b314390db39df326f6e571c6f468cc591
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-18 13:35:01 -0300

    added superuser restriction to /sell path

--- products/views.py
+++ products/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 
 from .forms import ProductForm 
-from .models import Product, Category
+from .models import Product
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def product_create_view(request):
-    my_form = ProductForm(request.POST, request.FILES)
-    if request.method == "POST":
-        if my_form.is_valid():
-            Product.objects.create(**my_form.cleaned_data)
 
-    context = {
-        'form': my_form 
-    }
-    
-    return render(request, 'products/product_create.html', context)
+    if request.user.is_superuser:
+        my_form = ProductForm(request.POST, request.FILES)
+        if request.method == "POST":
+            if my_form.is_valid():
+                Product.objects.create(**my_form.cleaned_data)
+
+        context = {
+            'form': my_form 
+        }
+
+        return render(request, 'products/product_create.html', context)
+    else:
+        return redirect('/')
