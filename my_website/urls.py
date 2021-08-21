92ef236bf412acf8d5cc40132c8d5f5684e6896e
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-21 16:47:39 -0300

    moved templates and static files to pages app.

--- my_website/urls.py
+++ my_website/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pages.views import home_view
-from products.views import product_create_view
-
 from django.conf import settings            #Images stuff
 from django.conf.urls.static import static  #Images stuff
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin_panel_page'),
-    path('', home_view, name='home_page'),
+    #Pages app
+    path('', include('pages.urls')),
     #Products app
     path('', include('products.urls')),
     # Accounts app
--- pages/views.py
+++ pages/views.py
@@ -5,11 +5,4 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
-    
-
-    return render(request, "home.html", {})
-
-'''if request.user.is_superuser == True:
-        return HttpResponse('You are super user.')
-    else:
-        return HttpResponse('Fuck you.')'''
+    return render(request, "pages/home.html", {})
