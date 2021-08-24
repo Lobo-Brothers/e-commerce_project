e2d4b4ecb4114d4e66a05c8098bad60465e4e77c
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-24 03:30:43 -0300

    moved customqueryset functions to another file in order to be mas prolijo perritooooooooooooo

---
+++ products/customqueryset.py
@@ -0,0 +1,35 @@
+class CustomQuerySet(models.QuerySet):
+
+    #Funciones para filtrar los productos dentro del template.
+    def on_slider(self):
+        return self.filter(preview='slider')
+
+    def on_preorder(self):
+        return self.filter(preview='preorder')
+
+    def get_accesories(self):
+        return self.filter(category='accesory')
+
+    def get_hoodies(self):
+        return self.filter(category='hoodie')
+
+    def get_bottoms(self):
+        return self.filter(category='bottom')
+
+    def get_sneakers(self):
+        return self.filter(category='sneaker')
+
+    def get_outerwears(self):
+        return self.filter(category='outerwear')
+
+    def get_tshirts(self):
+        return self.filter(category='t-shirt')
+
+
+    #Esta funcion devuelve un producto aleatorio de la lista de productos del queryset
+    def random(self):
+        random = self
+        if len(random) > 0:
+            return random[randint(0, len(random) - 1)]
+
+        #Hacer esto me tomo una noche entera y una lata de speed
--- products/models.py
+++ products/models.py
@@ -1,47 +1,11 @@
 from products.choices import CATEGORY_CHOICES, PREVIEW_CHOICES
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
-from random import randint
+
+from customqueryset import CustomQuerySet
 
 
 # Create your models here.
-class CustomQuerySet(models.QuerySet):
-
-    #Funciones para filtrar los productos dentro del template.
-    def on_slider(self):
-        return self.filter(preview='slider')
-
-    def on_preorder(self):
-        return self.filter(preview='preorder')
-
-    def get_accesories(self):
-        return self.filter(category='accesory')
-
-    def get_hoodies(self):
-        return self.filter(category='hoodie')
-
-    def get_bottoms(self):
-        return self.filter(category='bottom')
-
-    def get_sneakers(self):
-        return self.filter(category='sneaker')
-
-    def get_outerwears(self):
-        return self.filter(category='outerwear')
-
-    def get_tshirts(self):
-        return self.filter(category='t-shirt')
-
-
-    #Esta funcion devuelve un producto aleatorio de la lista de productos del queryset
-    def random(self):
-        random = self
-        if len(random) > 0:
-            return random[randint(0, len(random) - 1)]
-
-        #Hacer esto me tomo una noche entera y una lata de speed
-
-
 class Product(models.Model):
     title           = models.CharField(max_length=128)
     image           = models.ImageField(upload_to='images')
