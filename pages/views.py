d76fca25aadd10b33bce670404daddb42761dcbc
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-24 03:32:12 -0300

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 from products.models import Product
-from products.models import Category
-from random import randint
 
 def home_view(request):
     product = Product.objects.all()
