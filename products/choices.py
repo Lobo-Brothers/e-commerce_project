6c28001f411b80356e787eec4b6d36b6a95b28c3
newtonhead <75225173+NewtonHead@users.noreply.github.com>
2021-08-24 02:17:05 -0300

    updated product model

---
+++ products/choices.py
@@ -0,0 +1,14 @@
+PREVIEW_CHOICES = [
+    ('slider', 'Slider'),
+    ('pre-order', 'Pre-order'),
+    ('hidden', 'Hidden'),
+]
+
+CATEGORY_CHOICES = [
+    ('accesory', 'Accesory'),
+    ('bottom', 'Bottoms'),
+    ('hoodie', 'Hoodies'),
+    ('outerwear', 'Outerwears'),
+    ('sneaker', 'Sneakers'),
+    ('t-shirt', 'T-Shirts'),
+]
--- products/models.py
+++ products/models.py
@@ -1,25 +1,31 @@
+from products.choices import CATEGORY_CHOICES, PREVIEW_CHOICES
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
+
 # Create your models here.
 
-PREVIEW_CHOICES = [
-    ('1', 'Slider'),
-    ('2', 'Pre-order'),
-    ('3', 'Hidden'),
-]
+class CustomManager(models.Manager):
+    def on_slider(self):
+        return super(CustomManager, self).get_queryset().filter(preview=1)
+    
+    def on_preorder(self):
+        return super(CustomManager, self).get_queryset().filter(preview=2)
 
 class Product(models.Model):
-    title       = models.CharField(max_length=128)
-    image       = models.ImageField(upload_to='images')
-    description = models.TextField(blank=True, null=True)
-    category    = TreeForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=9)
-    featured    = models.BooleanField(default=False)
-    preview     = models.CharField(default='3', max_length=10, choices=PREVIEW_CHOICES)
+    title           = models.CharField(max_length=128)
+    image           = models.ImageField(upload_to='images')
+    description     = models.TextField(blank=True, null=True)
+    category        = models.CharField(max_length=16, choices=CATEGORY_CHOICES)
+    price           = models.DecimalField(decimal_places=2, max_digits=9)
+    featured        = models.BooleanField(default=False)
+    preview         = models.CharField(default='hidden', max_length=10, choices=PREVIEW_CHOICES)
+    custom_category = TreeForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
+
+    objects     = CustomManager
     
     def __str__(self):
-        return self.title
+        return f'{self.category}, {self.title}'
 
 class Category(MPTTModel):
     name    = models.CharField(max_length=50, unique=True)
